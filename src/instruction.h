626c3735377ca87fcd3ff7649adc59f0b6f3ac1e
Willian-AC <williancorrea.aluno@unipampa.edu.br>
2026-03-30 23:02:36 -0300

    Add SaveAssemblyMem function declaration

--- src/instruction.h
+++ src/instruction.h
@@ -7,3 +7,5 @@ void decodeInstruction(Instruction * instruction, const char *serializedBinary);
 void loadInstructionsOnMem();
 
 void convertToAssemblyInstruction(const Instruction * instruction, char *buffer);
+
+void SaveAssemblyMem ();
