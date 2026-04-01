9abda6c6e6de5f7b3ac13610a65c7514223c94b0
Willian-AC <williancorrea.aluno@unipampa.edu.br>
2026-04-01 16:41:47 -0300

    Define ULAOut structure in ula.h
    Add ULAOut structure to hold ULA output values.

--- src/ula.h
+++ src/ula.h
@@ -1,6 +1,12 @@
 #pragma once
 
+
 #include <stdint.h>
 #include "types.h"
+
+typedef struct {
+    bool zeroUla;
+    int8_t value;
+} ULAOut;
 
 ULAOut ula(int8_t input1, int8_t input2, int ulaControl);
