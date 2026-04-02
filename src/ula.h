a2fb7cf81bfd8ade6ac634c30130c295a4df8ed2
will-nuness <150326671+will-nuness@users.noreply.github.com>
2026-04-02 07:09:20 -0300

    Update ula.h

--- src/ula.h
+++ src/ula.h
@@ -1,12 +1,7 @@
 #pragma once
 
-
 #include <stdint.h>
 #include "types.h"
 
-typedef struct {
-    bool zeroUla;
-    int8_t value;
-} ULAOut;
 
 ULAOut ula(int8_t input1, int8_t input2, int ulaControl);
