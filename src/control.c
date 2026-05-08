d20c9daf00aad2edce9c2073edb315b0e6e22385
Willian-AC <williancorrea.aluno@unipampa.edu.br>
2026-05-08 11:53:12 -0300

    Update control.c

--- src/control.c
+++ src/control.c
@@ -3,6 +3,7 @@
 void makeControl(const unsigned int opcode, const unsigned int funct, Control *control) {
     switch (control->state) {
         case 0: // Busca
+            control->branch = 0;
             control->wrtMem = 0; control->wrtReg = 0;
             control->ulaSourceA = 0; control->ulaSourceB = 1;
             control->wrtIr = 1; 
@@ -12,6 +13,7 @@ void makeControl(const unsigned int opcode, const unsigned int funct, Control *c
             break;
 
         case 1: // Decodificação
+            control->branch = 0;
             control->wrtIr = 0; control->wrtPc = 0;
             control->ulaSourceA = 0; control->ulaSourceB = 2;
             switch (opcode) {
@@ -26,6 +28,7 @@ void makeControl(const unsigned int opcode, const unsigned int funct, Control *c
             break;
 
         case 2: // Execução / Cálculo de Endereço
+            control->branch = 0;
             control->ulaSourceA = 1; control->ulaSourceB = 2;
             if (opcode == LW_OPCODE) control->state = 3;
             else if (opcode == SW_OPCODE) control->state = 5;
@@ -33,32 +36,38 @@ void makeControl(const unsigned int opcode, const unsigned int funct, Control *c
             break;
 
         case 3: // Acesso Memória (LW)
+            control->branch = 0;
             control->ulaSourceA = 1; control->ulaSourceB = 2;
             control->state = 4; break;
 
         case 4: // Write-back (LW) - Fim da instrução
+            control->branch = 0;
             control->wrtReg = 1; control->regDst = 0; control->memToReg = 1;
             control->wrtPc = 1; control->pcSource = 0; // Avança o PC
             control->state = 0; break;
 
         case 5: // Acesso Memória (SW) - Fim da instrução
+            control->branch = 0;
             control->wrtMem = 1; control->wrtReg = 0;
             control->wrtPc = 1; control->pcSource = 0; // Avança o PC
             control->state = 0; break;
 
         case 6: // Write-back (ADDI) - Fim da instrução
+            control->branch = 0;
             control->wrtReg = 1; control->regDst = 0; control->memToReg = 0;
             control->wrtMem = 0;
             control->wrtPc = 1; control->pcSource = 0; // Avança o PC
             control->state = 0; break;
 
         case 7: // Execução Tipo-R
+            control->branch = 0;
             control->ulaSourceA = 1; control->ulaSourceB = 0;
             control->ulaControl = funct; control->wrtReg = 0; control->wrtMem = 0;
             control->state = 8;
             break;
 
         case 8: // Write-back Tipo-R - Fim da instrução
+            control->branch = 0;
             control->wrtReg = 1; control->regDst = 1; control->memToReg = 0;
             control->wrtMem = 0;
             control->wrtPc = 1; control->pcSource = 0; // Avança o PC
@@ -70,6 +79,7 @@ void makeControl(const unsigned int opcode, const unsigned int funct, Control *c
             control->state = 0; break;
 
         case 10: // Jump - Fim da instrução
+            control->branch = 0;
             control->wrtPc = 1; control->pcSource = 2;
             control->state = 0; break;
 
