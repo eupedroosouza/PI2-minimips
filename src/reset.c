d5e1f938acd641f5aa6c85eb52b5026b5b9ba868
Willian-AC <williancorrea.aluno@unipampa.edu.br>
2026-06-04 18:56:41 -0300

    resetRegisters reseta registradores intermediários

--- src/reset.c
+++ src/reset.c
@@ -4,6 +4,8 @@
 #include "encoding.h"
 #include "main.h"
 
+PipelineRegisters pipeline;
+
 void resetInstructions() {
     for (int i = 0; i < MEM_SIZE; i++) {
         memInstruction.instructions[i] = emptyInstruction;
@@ -12,12 +14,19 @@ void resetInstructions() {
 }
 
 void resetRegisters() {
+
     for (int i = 0; i < REG_SIZE; i++) {
         registers[i] = 0;
     }
 
     // PC Reset
     pc = 0;
+
+    // regs reset
+    pipeline.if_id = (IF_ID){0};
+    pipeline.id_ex = (ID_EX){0};
+    pipeline.ex_mem = (EX_MEM){0};
+    pipeline.mem_wb = (MEM_WB){0};
 }
 
 void resetData() {
